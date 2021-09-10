82852650bf9af40870c9a2d7988e778e9d084dfc
noorhuzaifa11 <noorhuzaifa11@gmail.com>
2021-09-10 18:43:13 +0500

    Initialized firebase database

--- Firebase integration/main.py
+++ Firebase integration/main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+import firebase_admin
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cred_obj = firebase_admin.credentials.Certificate('firebase-adminsdk.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': 'https://attendancetest-33ab4-default-rtdb.firebaseio.com/'
+})
